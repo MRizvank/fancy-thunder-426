b8f5964df3b3dc8d9e93f4c6a6897e4da7aa3dee
zeeshani26 <ilahiamaan606@gmail.com>
2023-01-22 20:52:17 +0530

    Done

--- scripts/viewall.js
+++ scripts/viewall.js
@@ -1,3 +1,4 @@
+let userLogin = JSON.parse(localStorage.getItem("login"));
 let backtoHOme = document.getElementById("logo-container")
 backtoHOme.addEventListener("click", () => {
     location.href = "index.html"
@@ -80,16 +81,16 @@ document.querySelector(".sellerContainer").addEventListener("click", () => {
 })
 
 //filtering
-let category = document.querySelectorAll(".category")
-for (let item of category) {
-    item.addEventListener("change", () => {
-        if (item.checked) {
-            console.log(item.value)
-        } else {
-            console.error("not")
-        }
-    })
-}
+// let category = document.querySelectorAll(".category")
+// for (let item of category) {
+//     item.addEventListener("change", () => {
+//         if (item.checked) {
+//             console.log(item.value)
+//         } else {
+//             console.error("not")
+//         }
+//     })
+// }
 
 
 
@@ -165,6 +166,58 @@ function sorted() {
 
 }
 
+
+
+
+//checking for login
+
+document.querySelector(".cartContainer").addEventListener("click", () => {
+    if (userLogin) {
+        location.href = "cart.html"
+    } else {
+        location.href = "signup.html"
+    }
+
+})
+
+let phone = JSON.parse(localStorage.getItem("Phn"));
+let myValue = JSON.parse(localStorage.getItem("enteredvalue"));
+let password = JSON.parse(localStorage.getItem("myotp"));
+let profileLoginContainer = document.querySelector(".profileHoverContainer");
+if (userLogin == true && password[password.length - 1] == myValue) {
+    profileLoginContainer.innerHTML = `
+           <h4 id="rkhello" >Hello User</h4>
+              <ph3 id="rkaccess">${phone}</h3>
+              <div class="profileSignUpBtn">
+                  <button id="rksignup">Log Out</button>
+              </div>
+              <h3 id="cart">
+                <i class="fa-solid fa-bag-shopping" id="bag"></i> My Cart
+              </h3>
+    `
+}
+if (userLogin) {
+    let logOutBtn = document.querySelector("#rksignup")
+    logOutBtn.addEventListener("click", () => {
+        userLogin = false;
+        location.href = "index.html"
+        localStorage.setItem("login", JSON.stringify(userLogin))
+    })
+
+}
+
+// access to cart
+let cartBtn = document.getElementById("cart")
+cartBtn.addEventListener("click", () => {
+    if (userLogin) {
+        location.href = "cart.html"
+    } else {
+        location.href = "signup.html"
+    }
+})
+
+
+
 function categoryfilter(){
 
     let saree=document.getElementById("saree");
@@ -175,7 +228,6 @@ function categoryfilter(){
     let jewellery=document.getElementById("jewellery");
 
     saree.addEventListener("change", () => {
-        console.log(productData)
         if (saree.checked) {
             let abcd = productData.filter((element) => {
                 if (element.category == "Sarees") {
@@ -260,7 +312,6 @@ function pricefilter(){
     let above500 = document.getElementById("a500");
 
     under149.addEventListener("change", () => {
-        console.log(productData)
         if (under149.checked) {
             let abcd = productData.filter((element) => {
                 if (element.original_price < 149) {
